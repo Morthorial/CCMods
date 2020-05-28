9ebbb917ba10142e035364bd5fc611fa91281bf4
Morthorial <61378045+Morthorial@users.noreply.github.com>
2020-05-28 17:26:22 +0100

    Update 20Wrinklers.js

--- 20Wrinklers.js
+++ 20Wrinklers.js
@@ -1,25 +1,39 @@
-20Wrinklers.launch = function(){
-	function inRect(x,y,rect)
-	{
-		//find out if the point x,y is in the rotated rectangle rect{w,h,r,o} (width,height,rotation in radians,y-origin) (needs to be normalized)
-		//I found this somewhere online I guess
-		var dx = x+Math.sin(-rect.r)*(-(rect.h/2-rect.o)),dy=y+Math.cos(-rect.r)*(-(rect.h/2-rect.o));
-		var h1 = Math.sqrt(dx*dx + dy*dy);
-		var currA = Math.atan2(dy,dx);
-		var newA = currA - rect.r;
-		var x2 = Math.cos(newA) * h1;
-		var y2 = Math.sin(newA) * h1;
-		if (x2 > -0.5 * rect.w && x2 < 0.5 * rect.w && y2 > -0.5 * rect.h && y2 < 0.5 * rect.h) return true;
-		return false;
+20Wrinkler.launch = function(){
+	20Wrinkler.init = function() {
+		function inRect(x,y,rect)
+		{
+			//find out if the point x,y is in the rotated rectangle rect{w,h,r,o} (width,height,rotation in radians,y-origin) (needs to be normalized)
+			//I found this somewhere online I guess
+			var dx = x+Math.sin(-rect.r)*(-(rect.h/2-rect.o)),dy=y+Math.cos(-rect.r)*(-(rect.h/2-rect.o));
+			var h1 = Math.sqrt(dx*dx + dy*dy);
+			var currA = Math.atan2(dy,dx);
+			var newA = currA - rect.r;
+			var x2 = Math.cos(newA) * h1;
+			var y2 = Math.sin(newA) * h1;
+			if (x2 > -0.5 * rect.w && x2 < 0.5 * rect.w && y2 > -0.5 * rect.h && y2 < 0.5 * rect.h) return true;
+			return false;
+		}
+		Game.getWrinklersMax=function()
+		{
+			var n=10;
+			if (Game.Has('Elder spice')) n+=10;
+			return n;
+		}
+		Game.wrinklers=[];
+		for (var i=0;i<20;i++)
+		{
+			Game.wrinklers.push({id:parseInt(i),close:0,sucked:0,phase:0,x:0,y:0,r:0,hurt:0,hp:Game.wrinklerHP,selected:0,type:0});
+		}
 	}
-	Game.getWrinklersMax=function()
-	{
-		var n=10;
-		if (Game.Has('Elder spice')) n+=10;
-		return n;
+}
+
+if(!20Wrinkler.isLoaded){
+	if(CCSE && CCSE.isLoaded){
+		20Wrinkler.launch();
 	}
-	Game.wrinklers=[];
-	for (var i=0;i<20;i++)
-	{
-		Game.wrinklers.push({id:parseInt(i),close:0,sucked:0,phase:0,x:0,y:0,r:0,hurt:0,hp:Game.wrinklerHP,selected:0,type:0});
+	else{
+		if(!CCSE) var CCSE = {};
+		if(!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+		CCSE.postLoadHooks.push(20Wrinkler.launch);
 	}
+}
